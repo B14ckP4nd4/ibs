a78fd180d457f0300ddc1168bd8fe3bed83023a3
B14ckP4nd4 <B14ckP4nd4@protonmail.com>
2019-10-31 18:45:58 +0100

    update Response and Create Login Method

--- src/IBSng.php
+++ src/IBSng.php
@@ -13,36 +13,58 @@
         private $cookie;
         private $request;
         private $agent;
+        protected $connected = false;
+        protected $adminURLSuffix;
 
-        public function __construct( string $ip ,int $port = null ,string $adminUsername = null,string $adminPassword = null)
+        public function __construct(string $ip, int $port = null, string $adminUsername = null, string $adminPassword = null, bool $autpLogin = true)
         {
             $this->ip = $ip;
-            $this->port = ( is_null($port) ) ? config('ibsng.IBSng_port') : $port;
-            $this->adminUsername = ( is_null($adminUsername) ) ? config('ibsng.default_admin_username') : $adminUsername;
-            $this->adminPassword = ( is_null($adminPassword) ) ? config('ibsng.default_admin_password') : $adminPassword;
-            $this->agent = ( is_null($adminPassword) ) ? config('ibsng.default_requests_agent') : $adminPassword;
+            $this->port = (is_null($port)) ? config('ibsng.IBSng_port') : $port;
+            $this->adminUsername = (is_null($adminUsername)) ? config('ibsng.default_admin_username') : $adminUsername;
+            $this->adminPassword = (is_null($adminPassword)) ? config('ibsng.default_admin_password') : $adminPassword;
+            $this->agent = (is_null($adminPassword)) ? config('ibsng.default_requests_agent') : $adminPassword;
 
             // set cooKie
-            $this->cookie = tempnam(sys_get_temp_dir(),"CURLCOOKIE");
+            $this->cookie = tempnam(sys_get_temp_dir(), "CURLCOOKIE");
+
+            // Set Admin URL Suffix
+            $this->adminURLSuffix = '/IBSng/admin/';
 
             // Hidden lib xml errors
             libxml_use_internal_errors(true);
 
+            if ($autpLogin) $this->login();
+
+
         }
 
-        private function login(){
-            $url = $this->ip . '/IBSng/admin/';
-            $postData['username'] = $this->username;
-            $postData['password'] = $this->password;
-            $output = $this->request($url, $postData, true);
-            if (strpos($output, 'admin_index') > 0) {
+
+        // get User By ID
+        public function getUserbyID(int $userID)
+        {
+            $url = $this->ip . $this->adminURLSuffix . 'user/user_info.php';
+            $GET = [
+                'user_id_multi' => $userID
+            ];
+            $response = $this->sendRequest($url, [], $GET, true);
+            var_dump($response);
+
+        }
+
+
+        private function login()
+        {
+            $url = $this->ip . $this->adminURLSuffix;
+            $postData = [];
+            $postData['username'] = $this->adminUsername;
+            $postData['password'] = $this->adminPassword;
+            $response = $this->sendRequest($url, $postData, [], true);
+            if (strpos($response, 'admin_index') > 0) {
+                $this->connected = true;
                 return true;
             }
             throw new \Exception ("Can't login to IBSng. Wrong username or password");
         }
-
-
-
 
         // Setter Methods
 
@@ -53,36 +75,65 @@
 
         // Setter For Facade
 
-        public function setSetver(string $ip ,int $port = null ,string $adminUsername = null,string $adminPassword = null)
+        public function setSetver(string $ip, int $port = null, string $adminUsername = null, string $adminPassword = null)
         {
             $this->ip = $ip;
-            $this->port = ( is_null($port) ) ? config('ibsng.IBSng_port') : $port;
-            $this->adminUsername = ( is_null($adminUsername) ) ? config('ibsng.default_admin_username') : $adminUsername;
-            $this->adminPassword = ( is_null($adminPassword) ) ? config('ibsng.default_admin_password') : $adminPassword;
+            $this->port = (is_null($port)) ? config('ibsng.IBSng_port') : $port;
+            $this->adminUsername = (is_null($adminUsername)) ? config('ibsng.default_admin_username') : $adminUsername;
+            $this->adminPassword = (is_null($adminPassword)) ? config('ibsng.default_admin_password') : $adminPassword;
         }
 
         // Send IBSng Requests
 
-        private function sendRequest($url, array $post = [], array $get = [], $cookies = null ){
-            if(empty($cookies)) { $cookies = $this->cookie; }
+        private function sendRequest($url, array $post = [], array $get = [], $header = false, $cookies = null)
+        {
+            if (empty($cookies)) {
+                $cookies = $this->cookie;
+            }
 
-            $this->request = curl_init($url);
+            // Set Handler
+            $this->request = curl_init();
+
+            // SetTimeOut
             curl_setopt($this->request, CURLOPT_CONNECTTIMEOUT, 0);
             curl_setopt($this->request, CURLOPT_TIMEOUT, 10);
+
+            // Set URl and PORT
             curl_setopt($this->request, CURLOPT_URL, $url);
             curl_setopt($this->request, CURLOPT_PORT, $this->port);
+
+            // Set Post PARAMS
             curl_setopt($this->request, CURLOPT_POST, true);
             curl_setopt($this->request, CURLOPT_POSTFIELDS, $post);
-            curl_setopt($this->request, CURLOPT_HEADER, $get);
-            curl_setopt($this->request, CURLOPT_RETURNTRANSFER, TRUE);
-//        curl_setopt($this->handler, CURLOPT_FOLLOWLOCATION, TRUE);
+
+            // SET Headers
+            if ($header) {
+                curl_setopt($this->request, CURLOPT_HEADER, $header);
+                curl_setopt($this->request, CURLOPT_HTTPHEADER, [
+                    "Content-type" => "application/json",
+                    "Accept" => "application/json",
+                    "User-Agent" => "ibs-jsonrpc",
+                    "Accept-Charset" => "utf-8",
+                    "Cache-Control" => "no-cache",
+                ]);
+            }
+
+            // Set User Agent
             curl_setopt($this->request, CURLOPT_USERAGENT, $this->agent);
-            curl_setopt($this->request, CURLOPT_SSL_VERIFYHOST, false);
-            curl_setopt($this->request, CURLOPT_SSL_VERIFYPEER, false);
+
+
+            // Set Cookies
             curl_setopt($this->request, CURLOPT_COOKIEFILE, $this->cookie);
             curl_setopt($this->request, CURLOPT_COOKIEJAR, $this->cookie);
 
-            $response = curl_exec($this->handler);
+            // other Request Settings
+            //curl_setopt($this->handler, CURLOPT_FOLLOWLOCATION, TRUE);
+            curl_setopt($this->request, CURLOPT_RETURNTRANSFER, TRUE);
+            curl_setopt($this->request, CURLOPT_SSL_VERIFYHOST, false);
+
+            $response = curl_exec($this->request);
+
+            return $response;
         }
 
 
